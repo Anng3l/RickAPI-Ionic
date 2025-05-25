7fa58ed8760e9de7c107c83fcf90adbc5068328d
Angel <151583275+Anng3l@users.noreply.github.com>
2025-05-25 13:54:27 -0500

    Capturas de pantalla en el readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,6 @@
 export const environment = {
   production: false,
   mifirebaseConfig: {
-    apiKey: "AIzaSyCHiHPii9eHjTdVTmwACnn0yUn6hvHaiew",
-    authDomain: "d-movil.firebaseapp.com",
-    projectId: "d-movil",
-    storageBucket: "d-movil.firebasestorage.app",
-    messagingSenderId: "630167418487",
-    appId: "1:630167418487:web:ddc4b0a256a0daa15f3872"
   }
 };
 
